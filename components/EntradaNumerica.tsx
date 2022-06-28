7d81cb96f31073fb085c93be113674814b013c0e
Diogo Sarti <diogosarti13@gmail.com>
2022-06-27 22:32:13 -0300

    GameFinalizado

---
+++ components/EntradaNumerica.tsx
@@ -0,0 +1,22 @@
+import styles from "../styles/EntradaNumerica.module.css"
+
+interface EntradaNumericaPorps {
+    text: string
+    value: number
+    onChange: (newValue: number) => void
+}
+
+export default function EntradaNumerica(props: EntradaNumericaPorps){
+    const dec = () => props.onChange(props.value - 1)
+    const inc = () => props.onChange(props.value + 1)
+    return(
+        <div className={styles.entradaNumerica}>
+            <span className={styles.text}>{props.text}</span>
+            <span className={styles.value}>{props.value}</span>
+            <div className={styles.botoes}>
+                <button className={styles.btn} onClick={inc}>+</button>
+                <button className={styles.btn} onClick={dec}>-</button>
+            </div>
+        </div>
+    )
+}
---
+++ pages/jogo/[portas]/[temPresente].tsx
@@ -0,0 +1,48 @@
+import styles from "../../../styles/Jogo.module.css"
+import Link from "../../../node_modules/next/link";
+import { useEffect, useState } from "react";
+import Porta from "../../../components/Porta";
+import { atualiarPortas, criarPortas } from "../../../functions/portas";
+import { useRouter } from "../../../node_modules/next/router";
+
+export default function Jogo(){
+    const router = useRouter()
+
+    const [valido, setValido] = useState(false)
+    const [portas, setPortas] = useState([])
+
+    useEffect(() => {
+        const portas = +router.query.portas
+        const temPresente = +router.query.temPresente
+
+        const qtdePortasValidas = portas >= 3 && portas <=14
+        const temPresenteValido = temPresente >= 1 && temPresente <= portas
+
+        setValido(qtdePortasValidas && temPresenteValido)
+    }, [portas])
+
+    useEffect(() => {
+        const portas = +router.query.portas
+        const temPresente = +router.query.temPresente
+        setPortas(criarPortas(portas, temPresente))
+    }, [router?.query])
+
+    function renderizarPortas(){
+        return portas.map(porta => {
+            return <Porta key={porta.numero} value={porta} onChange={novaPorta => setPortas(atualiarPortas(portas, novaPorta))}/>
+        })
+    }
+    return(
+        <>
+        <div id={styles.jogo}>
+            <div className={styles.portas}>
+                {valido? renderizarPortas() : <h2>Valores inválidos!</h2>}
+            </div>
+            <div className={styles.botoes}>
+                <Link href={'/'}><a href="">Reiniciar jogo</a></Link>
+            </div>
+            
+        </div>
+        </>
+    )
+}
